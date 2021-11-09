8697c16fba1adab5bd38ea9a813338a79006518a
Jackson Egan <jackson@sneagan.com>
2021-11-09 15:08:25 -0500

    Re-enable the API initialization even when Kafka is used so that the health check endpoint functions

--- main.go
+++ main.go
@@ -85,13 +85,13 @@ func main() {
 				return
 			}
 		}()
-	} else {
-		err = srv.ListenAndServe(serverCtx, logger)
+	}
 
-		if err != nil {
-			raven.CaptureErrorAndWait(err, nil)
-			logger.Panic(err)
-			return
-		}
+	err = srv.ListenAndServe(serverCtx, logger)
+
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		logger.Panic(err)
+		return
 	}
 }
