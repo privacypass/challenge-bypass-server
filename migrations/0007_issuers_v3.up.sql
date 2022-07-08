18ed3016f31f24c9c3e995a763fecc248759e7d8
husobee <husobee@e-ie.io>
2022-07-08 10:05:49 -0400

    fixing migration issues

--- migrations/0007_issuers_v3.up.sql
+++ migrations/0007_issuers_v3.up.sql
@@ -1,17 +1,3 @@
--- v3_issuer_keys - holds all sign/redeem keys for issuers v3
-CREATE TABLE v3_issuer_keys (
-    key_id uuid primary key default uuid_generate_v4(),
-    issuer_id uuid references v3_issuers(issuer_id),
-    created_at timestamp not null default now(),
-    start_at timestamp,
-    end_at timestamp,
-    signing_key text not null,
-    public_key text,
-    cohort smallint not null default 1
-);
--- lookups will be done on the public key
-CREATE index keys_public_key_idx on v3_issuer_keys(public_key);
-
 -- v3_issuer - new issuer structure
 CREATE TABLE v3_issuers(
     issuer_id uuid primary key default uuid_generate_v4(),
@@ -30,33 +16,43 @@ CREATE TABLE v3_issuers(
     duration text default null
 );
 
+-- v3_issuer_keys - holds all sign/redeem keys for issuers v3
+CREATE TABLE v3_issuer_keys (
+    key_id uuid primary key default uuid_generate_v4(),
+    issuer_id uuid references v3_issuers(issuer_id),
+    created_at timestamp not null default now(),
+    start_at timestamp,
+    end_at timestamp,
+    signing_key text not null,
+    public_key text,
+    cohort smallint not null default 1
+);
+-- lookups will be done on the public key
+CREATE index keys_public_key_idx on v3_issuer_keys(public_key);
+
 -- v1 migrations
 insert into v3_issuers (
     issuer_id, issuer_type, created_at, expires_at, last_rotated_at, valid_from,
     buffer, days_out, overlap, cohort_count, redemption_repository, version, max_tokens)
-values
-    select
-        issuer_id, issuer_type, created_at, expires_at, last_rotated_at, created_at,
-        1, 30*3, 0, 1, 'postgres', version, max_tokens
-    from issuers
-    where version = 1;
+select
+    id, issuer_type, created_at, expires_at, rotated_at, created_at,
+    1, 30*3, 0, 1, 'postgres', version, max_tokens
+from issuers
+where version = 1;
 
 -- v2 migrations
 insert into v3_issuers (
     issuer_id, issuer_type, created_at, expires_at, last_rotated_at, valid_from,
     buffer, days_out, overlap, cohort_count, redemption_repository, version, max_tokens)
-values
-    select
-        issuer_id, issuer_type, created_at, expires_at, last_rotated_at, created_at,
-        1, 30, 7, 1, 'dynamodb', version, max_tokens
-    from issuers
-    where version = 2;
+select
+    id, issuer_type, created_at, expires_at, rotated_at, created_at,
+    1, 30, 7, 1, 'dynamodb', version, max_tokens
+from issuers
+where version = 2;
 
 -- keys introduction
 insert into v3_issuer_keys (
     issuer_id, created_at, signing_key, cohort)
-values
-    select
-        issuer_id, created_at, signing_key, issuer_cohort
-    from issuers
-;
+select
+    id, created_at, signing_key, issuer_cohort
+from issuers;
