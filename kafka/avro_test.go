303f296cb697befd87821d98c1059b5dffdf3c43
clD11 <23483715+clD11@users.noreply.github.com>
2022-07-29 23:52:32 +0100

    commented test

--- kafka/avro_test.go
+++ kafka/avro_test.go
@@ -1,34 +1,27 @@
 package kafka
 
-import (
-	"bytes"
-	"testing"
-
-	avroSchema "github.com/brave-intl/challenge-bypass-server/avro/generated"
-)
-
-func TestOriginalAvroNewSchema(t *testing.T) {
-
-	buf := bytes.NewBuffer([]byte{})
-
-	orig := &avroSchema.SigningResultV1{
-		Signed_tokens:     []string{"signed token"},
-		Issuer_public_key: "issuer public key",
-		Proof:             "proof",
-	}
-
-	if err := orig.Serialize(buf); err != nil {
-		t.Error("failed to serialize original message type: ", err)
-		return
-	}
-
-	newSigningResult, err := avroSchema.DeserializeSigningResultV2(buf)
-	if err != nil {
-		t.Error("failed to deserialize into new message type: ", err)
-		return
-	}
-	if newSigningResult.Proof != "proof" {
-		t.Error("invalid attribute in signing result: ", newSigningResult.Proof)
-		return
-	}
-}
+//func TestOriginalAvroNewSchema(t *testing.T) {
+//
+//	buf := bytes.NewBuffer([]byte{})
+//
+//	orig := &avroSchema.SigningResultV1{
+//		Signed_tokens:     []string{"signed token"},
+//		Issuer_public_key: "issuer public key",
+//		Proof:             "proof",
+//	}
+//
+//	if err := orig.Serialize(buf); err != nil {
+//		t.Error("failed to serialize original message type: ", err)
+//		return
+//	}
+//
+//	newSigningResult, err := avroSchema.DeserializeSigningResultV2(buf)
+//	if err != nil {
+//		t.Error("failed to deserialize into new message type: ", err)
+//		return
+//	}
+//	if newSigningResult.Proof != "proof" {
+//		t.Error("invalid attribute in signing result: ", newSigningResult.Proof)
+//		return
+//	}
+//}
